89bd216bdadcc1bdd3f9f2638e55e97178d469d9
Ian Sanctos <barbosa.ian@gmail.com>
2018-02-21 03:05:55 +0000

    trabalho

--- aula1alex.c
+++ aula1alex.c
@@ -9,3 +9,11 @@ void main()
 saudacao();
 }
 
+
+//https://docs.google.com/forms/d/e/1FAIpQLSc1T74RCXdHacdJspJnxpnGzc1uj-0XgnVmhYQBy1Y0eQi5Ew/viewform
+//ian.barbosa@soulasalle.com.br
+//s@nctos131424
+//É uma subrotina, conjunto de comandos que realiza uma tarefa especifica em um modulo dependente da função.
+//Os parâmetros são as variáveis declaradas diretamente no cabeçalho da função.
+//Toda função em C precisa retornar algum valor para o compilador, à exceção de alguma função declarada para retornar algo do tipo void.
+//
