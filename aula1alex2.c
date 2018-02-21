03402089e45846362e562f9607dee1bf53a1d552
Ian Sanctos <barbosa.ian@gmail.com>
2018-02-21 02:40:48 +0000

    trabalho

---
+++ aula1alex2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int somanumeros (int, int);
+{
+    main()
+    int somanumeros (int, int);
+}
+{
+    int somanumeros (int num1, int num2);
+    printf("informe o primeiro numero:");
+    scanf("%d", &num1);
+    printf("informe o segundo numero:");
+    scanf("%d", &num2);
+    total=num1+num2;
+    printf("A soma de [%d] e [%d] = [%d]", num1, num2, total);
+}
