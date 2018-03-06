fcf009c4b7c3aee9d163454ac50debffde44d1e7
Ian Sanctos <barbosa.ian@gmail.com>
2018-03-06 00:25:26 +0000

    exercicio7

---
+++ exercicio7prog2alex.c
@@ -0,0 +1,28 @@
+//MANIPULACAO DE STRING
+/*1-Qual é a biblioteca de manipulação de strings da linguagem C ?
+#include <string.h>
+
+2-Qual é a função para “comparar” strings ?
+strcpy(string_destino, string_origem);
+Realiza a cópia do conteúdo de uma variável a outra.
+
+3-Qual é a função para “concatenar” strings ?
+strcat(string_destino, string_origem);
+
+Realiza a concatenação do conteúdo de uma variável a outra.
+Ambas devem ser strings.
+
+4-Qual é a função para descobrir o “tamanho” de uma string?
+strlen(vetor)
+
+5-Faça um programa em C que leia 2 nomes e diga:
+5.1-Se as strings são iguais ou diferentes
+5.2-Imprima os 2 nomes em ordem alfabética
+5.3-Imprima o tamanho de cada string
+5.4-Gere uma 3a. variável pela concatenação das 2 strings nome1 e nome2
+*/
+#include <stdio.h>
+#include <string.h>
+{
+    
+}
